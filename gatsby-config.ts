555bad241fd05af1739aa8559efe5a91d774e70a
Sten ter Stal <mail@stenterstal.com>
2022-04-04 19:41:36 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-sass"]
+};
+
+export default config;
