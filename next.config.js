f75aefa72fc4e70533aeb7cff3798bf0b5cadb55
Sten <mail@stenterstal.com>
2024-07-25 15:38:23 +0200

    No jekyll

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
+const isProd = process.env.NODE_ENV === 'production'
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
     output: 'export',
+    // assetPrefix: isProd ? '/' : '',
+    distDir: 'dist',
 
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
 
     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
     // skipTrailingSlashRedirect: true,
-
-    // Optional: Change the output directory `out` -> `dist`
-    distDir: 'dist',
 }
 
 module.exports = nextConfig
