0386648c84bd02e593b5a102789b177c4f126b5e
Sten <mail@stenterstal.com>
2024-08-06 15:34:10 +0200

    Updates

---
+++ _next/static/chunks/app/projects/[slug]/page-162d33bdde1857a0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[101],{1994:function(n,e,u){Promise.resolve().then(u.t.bind(u,5633,23))},5633:function(){}},function(n){n.O(0,[966,971,23,744],function(){return n(n.s=1994)}),_N_E=n.O()}]);
--- server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ta9fnaACulrJr6Xu8VId8Jqao3OxgHAYspa7ijkBBYc=\"\n}"
